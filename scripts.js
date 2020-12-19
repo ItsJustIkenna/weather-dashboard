4020edf854da493ea64c15ec8c494f5b0485102a
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2020-12-19 12:41:04 -0500

    created API call

--- scripts.js
+++ scripts.js
@@ -1,3 +1,28 @@
-$(function() {
-    console.log("hello World");
-})
+$(function () {
+  //Dom Variables
+  var LiEl = $("<li>");
+  var cardEl = $("<div>");
+  var search = $('#search');
+  //JavaScript Variables
+  var now = moment().format("MMMM Do YYYY, h:mm:ss a");
+  var API_key = "a2e3bbaa7fcbfdcff54e3b11604bb2fb";
+
+  //Function Definitions
+
+  function pageLoad() {}
+  //Function Calls
+  //Event Listeners
+  $("#btn").on("click", function (e) {
+    e.preventDefault();
+    var queryUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=london&appid=" +
+      API_key;
+
+      $.ajax({
+          url: queryUrl,
+          method: "GET",
+      }).then(function(response) {
+          console.log(response);
+      })
+  });
+});
